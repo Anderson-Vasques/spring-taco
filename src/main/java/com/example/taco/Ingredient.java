e392dfc64c3d3f2ddf47f99f64fbf4029dc55028
Anderson Rosa <anderson.vasques.da.rosa@gmail.com>
2021-01-06 23:07:31 -0300

    Added javax anotations - moving from jdbc to jpa

--- src/main/java/com/example/taco/Ingredient.java
+++ src/main/java/com/example/taco/Ingredient.java
@@ -1,11 +1,20 @@
 package com.example.taco;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
+
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- src/main/java/com/example/taco/Order.java
+++ src/main/java/com/example/taco/Order.java
@@ -1,6 +1,8 @@
 package com.example.taco;
 
 import lombok.Data;
+
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -8,13 +10,20 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
@@ -47,9 +56,15 @@ public class Order {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
         this.tacos.add(taco);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
     }
 }
